01ad8a19d61ec13a12ad1d131bc8cabf6d8779cf
sigdelsanjog <sigdelsanjog@gmail.com>
2018-01-11 15:52:09 +0545

    First script to read no of lines in a file and also append texts in a file

---
+++ basics/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+lines=`cat $1 | wc -l`
+echo The number of lines in the file $1 is $lines
+echo $0
+echo "random texts \br \new \line in a file 'ls -a' " >> $1
+
+random texts in a file 'ls -a' 
